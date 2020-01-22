65a6cfb65df5070d057a9a02606643860d8e8d08
Andrew <nesbettar@g.cofc.edu>
2020-01-22 11:56:00 -0500

    Adding helloWorld.java to the repo

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld{
+    
+    public static void hello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+}
+
+    
